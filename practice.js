b3526b8c6a567947a6c1b83e198ce7afca1a3359
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-12 12:48:33 -0500

    did numberOfSteps

--- practice.js
+++ practice.js
@@ -415,5 +415,12 @@ console.log(numJewelsInStones(J, S))
 // Output: 12
 
 var numberOfSteps  = function(num) {
-    
+    let steps = 0;
+    while(num > 0){
+        if(num % 2 === 0) num /= 2;
+        else --num;
+        ++steps;
+    }
+    return steps;
 };
+console.log(numberOfSteps(123));
