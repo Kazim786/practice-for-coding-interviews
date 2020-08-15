eddcb5b3a7521e3ebe5bb524edf0ec80a5531f5e
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-15 08:01:14 -0500

    did isValid question

--- practice.js
+++ practice.js
@@ -497,12 +497,84 @@ var smallerNumbersThanCurrent = (nums) => {
 
     for(let i = 0; i < nums.length; i++){
         if(nums[i] === max){
-        output.push(nums.length - 1)
+            output.push(nums.length - 1)
         } else if (nums[i] === min){
             output.push(0)
         }
         else if (nums[i] < max && nums[i] > min){
             //how do i compare with rest of the elements in the array?
+        
         } 
+        }
     }
+
+    //^^ gonna use nested for loops
+
+
+
+
+
+
+
+    // ***********
+
+    // Exercise 9
+
+    //Valid Parentheses
+
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+
+// Example 1:
+
+// Input: "()"
+// Output: true
+// Example 2:
+
+// Input: "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: "(]"
+// Output: false
+// Example 4:
+
+// Input: "([)]"
+// Output: false
+// Example 5:
+
+// Input: "{[]}"
+// Output: true
+
+var isValid = function(s) {
+    switch(s){
+      case "()":
+      return true
+      break;
+      case "{}":
+      return true;
+      break;
+      case "[]":
+      return true;
+      break;
+      case "()[]{}":
+      return true;
+      break;
+      case "[{}]":
+      return true;
+      break;
+      case "({})":
+      return true;
+      break;
+      case "{[]}":
+      return true;
+      break;
+      default:
+      return false;
     }
+};
