7b37f5c67f096465a888f5cd0e0d7c865a21d720
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-17 14:18:53 -0500

    best time to buy and sell stock

--- practice.js
+++ practice.js
@@ -692,7 +692,7 @@ function fizzBuzz(n) {
             str1 = str1.toLowerCase()
             str2 = str2.toLowerCase()
             const frequency1 = {}
-            const frequency2 = {}
+            // const frequency2 = {}
 
             //if lengths of string isnt the same automatically make it false
 
@@ -709,6 +709,7 @@ function fizzBuzz(n) {
                     frequency1[letter]++
                 }
             }
+
             for(let letter of str2){
                 if(!frequency2[letter]){
                     frequency2[letter] = 1
@@ -732,4 +733,34 @@ function fizzBuzz(n) {
             }
 
         }
+
+
+        //****************** */
+
+
+        // 121. Best Time to Buy and Sell Stock
+
+//         Say you have an array for which the ith element is the price of a given stock on day i.
+
+// If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+// Note that you cannot sell a stock before you buy one.
+
+// Example 1:
+
+// Input: [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//              Not 7-1 = 6, as selling price needs to be larger than buying price.
+// Example 2:
+
+// Input: [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+        var maxProfit = function(prices) {
+            //selling price > buying price
+            //you can only buy 1 and sell 1 stock
+            //if no transaction is done the max-profit should return 0
+        };
 
