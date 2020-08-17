a6c5d55094e1fee4c1f86a7784b89ca60b5c0887
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-17 13:04:09 -0500

    fizzbuzz

--- practice.js
+++ practice.js
@@ -649,3 +649,29 @@ function isHappyNumber(number)
 
 
 isHappyNumber(num)
+
+//*********************** */
+
+//FizzBuzz Corrected
+
+function fizzBuzz(n) { 
+  
+    for(let i = 1; i <= n; i++){ 
+      
+       if(i % 15 === 0){ 
+         console.log('fizzbuzz') 
+         }
+         else if(i % 5 === 0){ 
+           console.log("buzz") 
+           }  
+           else if (i % 3 === 0){ 
+             console.log('fizz') 
+             } 
+             else { 
+               console.log(i) 
+               } 
+               }  
+               }
+    
+    
+    fizzBuzz(30)
