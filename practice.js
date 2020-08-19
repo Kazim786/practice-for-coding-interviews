73aba55f7b59844300f2bebffae793230472ae3b
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-19 18:40:17 -0500

    added corrected version of my code

--- practice.js
+++ practice.js
@@ -1041,32 +1041,33 @@ console.log(isValid('(]'))
 
 //Corrected version of my code for isValid parenthesis: 
 
-const isValid = (parenthesis) => {
-    if(typeof(parenthesis) !== "string" ){
-        return false
-    }
-    const stack = []
-    const mapClosingBrackets = {
-        "{":"}",
-        "[":"]",
-        "(":")"
-    }
-    const characters = parenthesis.split("")
-    for(let i = 0; i < characters.length; i++){
-        if(characters[i] === "{" || characters[i] === '[' || characters[i] === '(' ){
-            stack.push(characters[i])
-        }
-        else if(characters[i] === "}" || characters[i] === ']' || characters[i] === ')' ){
-            let poppedValue = stack.pop()
-            if(mapClosingBrackets[poppedValue]!=characters[i]){
-                return false
-            }
-        }
+const isValid = (str) => {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
     }
 
-    if(stack.length) return false
-    return true
-    }  
+    for (let i = 0; i < str.length; i++) {
+
+        // If character is an opening brace add it to a stack
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+            stack.push(str[i]);
+        }
+        //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
+        else {
+            let last = stack.pop();
+
+            //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
+            if (str[i] !== map[last]) {return false};
+        }
+    }
+    // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
+        if (stack.length !== 0) {return false};
+
+    return true;
+    }
 
 //LINE 611 AND 554 HAVE THE HAPPY NUMBER AND VALID PARENTHESIS QUESTIONS
 //**************************** */
