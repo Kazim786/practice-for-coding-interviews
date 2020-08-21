8b50afcd5144d3b1a60c3f04f8edab13b11bbbb5
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-21 13:59:05 -0500

    removed duplicates from the array

--- practice.js
+++ practice.js
@@ -1146,3 +1146,17 @@ const isHappyNumber = (number) =>
 
 
 isHappyNumber(num)
+
+
+//************** */
+
+// Remove Duplicates from Sorted Array
+
+// Given a sorted array nums, remove the duplicates 
+// in-place such that each element appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this
+//  by modifying the input array in-place with O(1) extra memory.
+
+//Plan of action: Make a mapped array and return its length
+
