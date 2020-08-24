3e59e23adc3d38ca1247d1af7ccbfe39a2da53ef
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-24 11:01:18 -0500

    diddid

--- practice.js
+++ practice.js
@@ -1327,3 +1327,57 @@ function MaxProfitInStockMarket(stockPrice)
 }
 
 MaxProfitInStockMarket(stockList)
+
+//**************** */
+
+const breakPalindrome = (palindrome) => {
+    if(palindrome.length === 1){
+        return ""
+    } 
+  const alphabetArr = ("abcdefghijklmnopqrstuvwxyz").split("")
+
+
+  let palindrome2 = ''
+
+  for(let i = 0; i < palindrome.length; i++){
+     
+       //have to replace with letter that isnt in the string so to avoid palindrome
+      // console.log([char])
+      if(palindrome[1] !== alphabetArr[i]){
+        palindrome2 = palindrome.replace(palindrome[1], alphabetArr[i])
+      }
+      
+    }
+    return palindrome2
+  }
+ 
+
+  //*********** */
+
+  const isValid = (str) => {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    for (let i = 0; i < str.length; i++) {
+
+        // If character is an opening brace add it to a stack
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+            stack.push(str[i]);
+        }
+        //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
+        else {
+            let last = stack.pop();
+
+            //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
+            if (str[i] !== map[last]) {return false};
+        }
+    }
+    // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
+        if (stack.length !== 0) {return false};
+
+    return true;
+    }
