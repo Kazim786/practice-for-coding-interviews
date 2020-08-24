b1d51e79e4743d9da20856783ee915b00a263c60
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-24 13:13:47 -0500

    if k === 1

--- practice.js
+++ practice.js
@@ -1381,3 +1381,37 @@ const breakPalindrome = (palindrome) => {
 
     return true;
     }
+
+
+
+    //***************** */
+
+    //215. Kth Largest Element in an Array
+
+    //Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Example 1:
+
+// Input: [3,2,1,5,6,4] and k = 2
+// Output: 5
+// Example 2:
+
+// Input: [3,2,3,1,2,4,5,5,6] and k = 4
+// Output: 4
+
+
+
+
+    var findKthLargest = function(nums, k) {
+        //pop the k number of large elements that precede the kth element
+        if(k > 1){
+            //find max number and slice array
+            //lets say k is 2. We slice array one time so the 2nd largest becomes the largest number in the array
+
+        } else if (k === 1){
+            var max = nums.reduce(function(a, b) {
+                return Math.max(a, b);
+            });
+            return max
+        }
+    };
