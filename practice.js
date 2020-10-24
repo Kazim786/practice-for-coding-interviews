1f791c1412b8c230153ad84dbe735b861e7de2ab
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-10-23 19:43:06 -0500

    did leetcode

--- practice.js
+++ practice.js
@@ -1621,3 +1621,51 @@ const capsFirstLetter = (str) => {
   //******* */
 
 
+//leetcode practice
+
+
+//https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+
+//Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+
+
+//Input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
+
+
+
+
+const subtractProductAndSum = (n) => {
+  let nString =  n.toString()
+  let multiplied = 0;
+  let added = 0;
+  let sum = 0;
+ 
+  for(let i = 0; i < nString.length; i++){
+   if(multiplied === 0 && added === 0)
+   {
+     multiplied = Number(nString[i])
+   
+     added = Number(nString[i])
+        
+ 
+   }
+   else
+   {
+     multiplied *= Number(nString[i])
+     added += Number(nString[i])
+   }
+ 
+  }
+  sum = multiplied - added
+  console.log(sum)
+  return sum
+ }
+ 
+ subtractProductAndSum(234)
